b778aedc504dc15389f0c5d6525a9248ac0e58a6
Nathalie Chan King Choy <nathalie.ckc.123@gmail.com>
2019-01-02 11:27:36 -0800

    Implement #3 Smart Contracts: Implement registering of new users
    Create onlyAdmin functions that add the new addresses to the corresponding mappings

--- contracts/AcmeWidgetCo.sol
+++ contracts/AcmeWidgetCo.sol
@@ -46,10 +46,32 @@ contract AcmeWidgetCo {
 
     // Functions - Within a grouping, place the view and pure functions last
     // constructor
+    constructor() public {
+        // The first admin is the deployer of the contract
+        adminList[msg.sender] = true;
+    }
+
+
 
     // fallback function (if exists)
     // external
     // public
+    function registerAdmin(address _newAdmin) public onlyAdmin {
+        adminList[_newAdmin] = true;
+    }
+
+    function registerTester(address _newTester) public onlyAdmin {
+        testerList[_newTester] = true;
+    }
+
+    function registerSalesDistributor(address _newSalesDistributor) public onlyAdmin {
+        salesDistributorList[_newSalesDistributor] = true;
+    }
+
+    function registerCustomer(address _newCustomer) public onlyAdmin {
+        customerList[_newCustomer] = true;
+    }
+
     // internal
     // private
 
