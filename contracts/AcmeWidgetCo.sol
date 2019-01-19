b5044b1a61660bcd09b0baa4660ec2fa285d0612
Nathalie C. Chan King Choy <nathalie.ckc.123@gmail.com>
2019-01-19 15:52:05 -0800

    Grading: Commenting using natspec (#37)
    Add comments according to Ethereum natural specification:
    https://github.com/ethereum/wiki/wiki/Ethereum-Natural-Specification-Format
    
    I think that's what was meant when the project requirements pointed us
    to:
    https://solidity.readthedocs.io/en/v0.4.21/layout-of-source-files.html#comments
    
    Signed-off-by: Nathalie C. Chan King Choy <nathalie.ckc.123@gmail.com>

--- contracts/AcmeWidgetCo.sol
+++ contracts/AcmeWidgetCo.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.4.24;
 import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 import './SafeMath32.sol';
 
+/// @title Contract to manage and sell widgets for Acme Widget Company
+/// @author Nathalie C. Chan King Choy
 contract AcmeWidgetCo {
     using SafeMath for uint256;
     using SafeMath32 for uint32;
@@ -10,14 +12,21 @@ contract AcmeWidgetCo {
     //===========================================
     // Struct definitions
     //===========================================
+    // The information about a single widget
     struct WidgetData {
       uint32 serialNumber;
       uint8 factoryMadeAt;
       uint8 siteTestedAt;
       uint32 testResults;  // bit == 1 => that test passed, 0 => test failed
+      // e.g. testResults == 0xFFFFFFFF means all 32 tests passed
+      // e.g. testResults == 0xFFFF0000 means only the first 16 tests passed
     }
 
-    // Assumption: Customers don't buy 1 widget from a given bin, they buy lots
+    // Assumption: Customer will buy >1 widget in an order.
+    // The information for an order of widgets from a particular bin.
+    // Widgets are sold in array index order
+    // e.g. Customer buys 5 widgets. If [1] was the last widget sold in that
+    //      bin, then firstIndex is [2], lastIndex is [6] for this order.
     struct WidgetOrderFill {
       uint8 bin;
       // Indices into corresponding bin of widgets
@@ -34,18 +43,21 @@ contract AcmeWidgetCo {
     // Lists of users by role
     mapping (address => uint8) public addr2Role;
     // Enum may seem appropriate, but you have to document the decoding anyway
-    // because the enum isn't accessible in JavaScript, so skip using enum
+    // because the enum isn't accessible in JavaScript, so skip using enum.
+    // This is how to decode:
     // Admin = 1
     // Tester = 2
     // Sales Distributor = 3
     // Customer = 4
 
+
+    // Assumption: During the life of this contract Acme won't ever reach >256 sites
     // Stores names of factories and test sites
-    // During the life of this contract Acme won't ever reach >256 sites
     string[] public factoryList;
     string[] public testSiteList;
     uint8 public factoryCount;
     uint8 public testSiteCount;
+
 
     // Used to ensure we don't add a duplicate factory or test site
     // Uses the keccak256 hash of the string for the lookup
@@ -53,9 +65,11 @@ contract AcmeWidgetCo {
     mapping (bytes32 => uint8) public factoryMapping;
     mapping (bytes32 => uint8) public testSiteMapping;
 
+
     // Track the widgets. Map the serial number to position in widgetList
     // Assumption: We won't have more than 4 billion widgets
-    // Bin0 is unsellable widgets, so not all 0 indices are populated b/c N/A
+    // Bin0 is unsellable widgets (i.e. did not have the correct sets of
+    //   passing test results. So, not all 0 indices are populated b/c N/A.
     // Bin1 has most functionality, Bin2 a bit less, Bin3 even less
     WidgetData[] public widgetList;
     mapping (uint32 => uint32) public widgetSerialMapping;
@@ -67,9 +81,10 @@ contract AcmeWidgetCo {
     uint32[] public bin3Widgets;
     uint32[4] public binWidgetCount;
     // HACK: Because using uint instead of int, widget[0] never really gets sold
-    // Deal with that later if time allows
-    uint32[4] public lastWidgetSoldInBin; // Index of last sold in bin
+    // TODO: Deal with that later if time allows
+    uint32[4] public lastWidgetSoldInBin; // Index of last sold in that bin
     mapping (address => WidgetOrderFill[]) public customerWidgetMapping; // Who owns each widget in widgetList
+
 
     //===========================================
     // Events
@@ -84,6 +99,7 @@ contract AcmeWidgetCo {
     event NewUnitPrice(uint8 indexed _bin, uint256 _newPrice, address indexed _salesDistributor);
     event NewBinMask(uint8 indexed _bin, uint32 _newBinMask, address indexed _salesDistributor);
     event WidgetSale(uint8 indexed _bin, uint32 _quantity, address indexed _customer, uint256 _totalAmtPaid);
+
 
     //===========================================
     // Modifiers
@@ -130,11 +146,10 @@ contract AcmeWidgetCo {
     }
 
 
-    // Functions - Within a grouping, place the view and pure functions last
-
     //===========================================
     // constructor
     //===========================================
+    /// @notice Set up initial conditions for the contract, on deployment
     constructor() public {
         // Circuit breaker
         stopContract = false;
@@ -152,7 +167,6 @@ contract AcmeWidgetCo {
     }
 
     // fallback function (if exists)
-    // external
 
     //===========================================
     // public
@@ -161,41 +175,58 @@ contract AcmeWidgetCo {
     //-------------------------
     // Admin functions
     //-------------------------
-    // Circuit breaker
+
+    /// @notice Circuit breaker enable - start emergency mode
     function beginEmergency() public onlyAdmin {
         stopContract = true;
     }
 
+    /// @notice Circuit breaker disable - end emergency mode
     function endEmergency() public onlyAdmin {
         stopContract = false;
     }
 
     // Functions to add to user lists
+
+    /// @notice Admin can add another admin
+    /// @dev Admin has privileges for adding users, sites, or stopping contract
+    /// @param _newAdmin is Ethereum address of the new admin
     function registerAdmin(address _newAdmin) public onlyAdmin {
         require(addr2Role[_newAdmin] == 0);
         addr2Role[_newAdmin] = 1;
         emit NewAdmin(_newAdmin);
     }
 
+    /// @notice Admin can add another tester
+    /// @dev Tester has privileges to record test results for widgets
+    /// @param _newTester is Ethereum address of the new tester
     function registerTester(address _newTester) public onlyAdmin {
         require(addr2Role[_newTester] == 0);
         addr2Role[_newTester] = 2;
         emit NewTester(_newTester);
     }
 
+    /// @notice Admin can add another sales distributor
+    /// @dev Sales dist. has privileges to update bin masks and unit prices
+    /// @param _newSalesDistributor is Ethereum address of the new sales dist.
     function registerSalesDistributor(address _newSalesDistributor) public onlyAdmin {
         require(addr2Role[_newSalesDistributor] == 0);
         addr2Role[_newSalesDistributor] = 3;
         emit NewSalesDistributor(_newSalesDistributor);
     }
 
+    /// @notice Admin can add another customer
+    /// @dev Customer has privileges to buy widgets
+    /// @param _newCustomer is Ethereum address of the new customer
     function registerCustomer(address _newCustomer) public onlyAdmin {
         require(addr2Role[_newCustomer] == 0);
         addr2Role[_newCustomer] = 4;
         emit NewCustomer(_newCustomer);
     }
 
-    // Won't be added if factory is already in the list
+    /// @notice Admin can add another factory for tester to choose from
+    /// @dev Won't be added if _factory is already in the list
+    /// @param _factory is the name of the new factory.
     function addFactory(string _factory) public onlyAdmin {
         require(factoryCount < 255);  // Prevent overflow
         require(factoryMapping[keccak256(abi.encodePacked(_factory))] == 0);
@@ -207,7 +238,9 @@ contract AcmeWidgetCo {
         emit NewFactory(factoryCount, _factory);
     }
 
-    // Won't be added if test site is already in the list
+    /// @notice Admin can add another test site for tester to choose from
+    /// @dev Won't be added if _testSite is already in the list
+    /// @param _testSite is the name of the new test site.
     function addTestSite(string _testSite) public onlyAdmin {
         require(testSiteCount < 255);  // Prevent overflow
         require(testSiteMapping[keccak256(abi.encodePacked(_testSite))] == 0);
@@ -219,10 +252,23 @@ contract AcmeWidgetCo {
         emit NewTestSite(testSiteCount, _testSite);
     }
 
+
     //-------------------------
     // Tester functions
     //-------------------------
 
+    /// @notice Tester records the factory where the widget was made, the site
+    ///   where it was tested, and the test results for a given widget serial #
+    /// @dev Won't be added if serial # is already in the list
+    /// @dev TODO: Generalize to N bins if time allows
+    /// @dev TODO: Figure out 2-D arrays if time allows
+    /// @param _serial is the serial number for the widget under test
+    /// @param _factory is the factory where the widget was made
+    /// @param _testSite is the site where the widget was tested
+    /// @param _results is the bit mapping of what tests passed or failed
+    ///   bit == 1 => that test passed, 0 => test failed
+    ///   e.g. testResults == 0xFFFFFFFF means all 32 tests passed
+    ///   e.g. testResults == 0xFFFF0000 means only the first 16 tests passed
     function recordWidgetTests(uint32 _serial, uint8 _factory, uint8 _testSite, uint32 _results)
         public
         onlyTester
@@ -230,7 +276,7 @@ contract AcmeWidgetCo {
         require(_factory < factoryCount);           // Valid factory
         require(_testSite < testSiteCount);         // Valid test site
         require(widgetSerialMapping[_serial] == 0); // Widget not already recorded
-        
+
         uint8 bin;
         WidgetData memory w;
         w.serialNumber = _serial;
@@ -240,8 +286,6 @@ contract AcmeWidgetCo {
         widgetList.push(w);
         widgetSerialMapping[_serial] = widgetCount; // Save index for serial #
 
-        // HACK: Generalize to N bins if time allows
-        // HACK: Figure out 2-D arrays if time allows
         if ((_results & binMask[1]) == binMask[1]) {
             bin1Widgets.push(widgetCount);
             bin = 1;
@@ -263,24 +307,42 @@ contract AcmeWidgetCo {
     //-------------------------
     // Sales distributor functions
     //-------------------------
-    // HACK: Later generalize to N bins, if time allows
-    // Allow sales distributor to update the unit price of any of the bins
+
+    /// @notice Sales distributor can update the unit price of any of the bins
+    /// @dev TODO: Later generalize to N bins, if time allows
+    /// @param _bin is the bin whose price is getting updated
+    /// @param _newPrice is the new price in wei
     function updateUnitPrice(uint8 _bin, uint256 _newPrice) public onlySalesDistributor {
         require((_bin > 0) && (_bin <=3), "Bin must be between 1 to 3, inclusive");
         binUnitPrice[_bin] = _newPrice;
         emit NewUnitPrice(_bin, _newPrice, msg.sender);
     }
 
+    /// @notice Sales distributor can update the mask of any of the bins
+    /// @dev TODO: Later generalize to N bins, if time allows
+    /// @dev Mask is how we know what bin a widget belongs in.  Widget must have
+    ///   a 1 in its test result in all positions where mask is 1 to get into
+    ///   that particular bin
+    /// @param _bin is the bin whose mask is getting updated
+    /// @param _newMask is the new mask value (e.g. 0xFFFFFF00)
     function updateBinMask(uint8 _bin, uint32 _newMask) public onlySalesDistributor {
         require((_bin > 0) && (_bin <=3), "Bin must be between 1 to 3, inclusive");
         binMask[_bin] = _newMask;
         emit NewBinMask(_bin, _newMask, msg.sender);
     }
 
+
     //-------------------------
     // Customer functions
     //-------------------------
-    // HACK: Generalize to N bins if time allows
+
+    /// @notice Function for customer to buy widgets from a specific bin
+    /// @dev This function is stopped by the circuit breaker
+    /// @dev TODO: Later generalize to N bins, if time allows
+    /// @dev TODO: Currently doesn't refund any excess if customer overpaid
+    /// @dev TODO: Currently only way to withdraw funds is when killing the contract.
+    /// @param _bin is the bin from which the customer wants to buy widgets
+    /// @param _quantity is the number of widgets to buy
     function buyWidgets(uint8 _bin, uint32 _quantity) payable public onlyCustomer stopInEmergency {
         require(_quantity > 0, "Must purchase >0 widgets.");
         require((_bin > 0) && (_bin <=3), "Bin must be between 1 to 3, inclusive");
@@ -291,7 +353,6 @@ contract AcmeWidgetCo {
         require((_quantity <= stock), "Insufficient stock. NOTE: widget[0] in each bin is not for sale.");
         require((uint256(_quantity).mul(uPrice) <= msg.value), "Insufficient funds.");
 
-        // HACK: Currently doesn't refund any excess if customer overpaid
         WidgetOrderFill memory w;
         w.bin = _bin;
         w.firstIndex = lastSold.add(1);
@@ -302,9 +363,5 @@ contract AcmeWidgetCo {
 
         emit WidgetSale(_bin, _quantity, msg.sender, msg.value);
     }
-
-
-    // internal
-    // private
 
 }
