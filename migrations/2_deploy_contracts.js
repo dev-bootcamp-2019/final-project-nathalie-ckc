23902ae258b874132caa15c227bc1feaa27d3ead
Nathalie C. Chan King Choy <nathalie.ckc.123@gmail.com>
2019-01-20 00:15:25 -0800

    Testnet: Add SafeMath32 to migration
    I had to make SafeMath32 since SafeMath is for uint256, and I wanted to
    use both.
    
    It works as-is on ganache, but I think I will need to link SafeMath32
    when using Rinkeby.
    
    Signed-off-by: Nathalie C. Chan King Choy <nathalie.ckc.123@gmail.com>

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
+var SafeMath32 = artifacts.require("SafeMath32");
 var AcmeWidgetCo = artifacts.require("AcmeWidgetCo");
 
 module.exports = function(deployer) {
+  deployer.deploy(SafeMath32);
+  deployer.link(SafeMath32, AcmeWidgetCo);
   deployer.deploy(AcmeWidgetCo);
 };
